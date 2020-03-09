45c58888135cbe8a3c1722c67aa0c0dbb6696b93
galaxy4276 <galaxyhi4276@gmail.com>
2020-03-10 06:07:36 +0900

    markdown add

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # DDCTUBE
 
 ### Nomad Coder에서 학습하여 제작중인 프로젝트 입니다.
+
+## Pages:
+-[] Home
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,12 +1,27 @@
-export const home = (req, res) => res.render('home');
-export const search = (req, res) => res.render('search');
+export const home = (req, res) => res.render('home', { pageTitle: 'Home' });
+
+export const search = (req, res) => {
+  // const { searchingBy } = req.query.term; // 작동안함.
+
+  // const {
+  //   query: { term: searchingBy }
+  // } = req;
+
+  const { term: searchingBy } = req.query;
+
+  res.render('search', { pageTitle: 'Search', searchingBy: searchingBy });
+}
 
 
 // videoRouter.js
 
-export const videos = (req, res) => res.render('videos');
-export const upload = (req, res) => res.render('upload');
-export const videoDetail = (req, res) => res.render('videoDetail');
-export const editVideo = (req, res) => res.render('edit Video');
-export const deleteVideo = (req, res) => res.render('deleteVideo');
+export const videos = (req, res) => res.render('videos', { pageTitle: 'Videos' });
+
+export const upload = (req, res) => res.render('upload', { pageTitle: 'Upload' });
+
+export const videoDetail = (req, res) => res.render('videoDetail', { pageTitle: 'Video Detail' });
+
+export const editVideo = (req, res) => res.render('edit Video', { pageTitle: 'Edit Video' });
+
+export const deleteVideo = (req, res) => res.render('deleteVideo', { pageTitle: 'Delete Video' });
 
