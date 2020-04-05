92188509965ba23e43882b0daf9fe34106ee230a
galaxy4276 <galaxyhi4276@gmail.com>
2020-04-05 16:48:21 +0900

    비밀번호 변경 기능 추가

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import routes from '../routes';
 import {
-    userDetail, changePassword, getEditProfile, postEditProfile, 
+    userDetail, getChangePassword, getEditProfile, postEditProfile, postChangePassword, 
 } from '../controllers/userController';
 import { onlyPrivate, uploadAvatar } from '../middlewares/middlewares';
 
@@ -11,7 +11,8 @@ const userRouter = express.Router();
 userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar, postEditProfile);
 
-userRouter.get(routes.changePassword, onlyPrivate, changePassword);
+userRouter.get(routes.changePassword, onlyPrivate, getChangePassword);
+userRouter.post(routes.changePassword, onlyPrivate, postChangePassword);
 
 userRouter.get(routes.userDetail(), userDetail);
 
