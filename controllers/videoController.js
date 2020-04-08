7e158045a93b68abdd08f3f60fcca59c69e7e4fa
galaxy4276 <galaxyhi4276@gmail.com>
2020-04-08 23:55:58 +0900

    api comment #2 코멘트 백엔드

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -6,7 +6,7 @@
 /* eslint-disable no-underscore-dangle */
 import routes from '../routes';
 import Video from '../models/Video';
-
+import Comment from '../models/Comment';
 
 export const home = async (req, res) => {
     try {
@@ -68,7 +68,7 @@ export const videoDetail = async (req, res) => {
         params: { id },
     } = req;
     try {
-        const video = await Video.findById(id).populate('creator');
+        const video = await (await Video.findById(id).populate('creator')).populate('comments');
         console.log(`finded Video: ${video}`);
         res.render('videoDetail', { pageTitle: `${video.title} | DDCtube`, video });
     } catch (error) {
@@ -149,3 +149,25 @@ export const postRegisterView = async (req, res) => {
         res.end();
     }
 };
+
+export const postAddComment = async (req, res) => {
+    const {
+        params: { id },
+        body: { comment },
+        user,
+    } = req;
+    
+    try {
+        const video = await Video.findById(id);
+        const newComment = await Comment.create({
+            text: comment,
+            creator: user.id,
+        });
+        video.comment.push(newComment._id);
+        video.save();
+    } catch (error) {
+        res.status(400);
+    } finally {
+        res.end();
+    }
+} 
--- routes.js
+++ routes.js
@@ -36,6 +36,7 @@ const KAKAO_CALLBACK = '/auth/kakao/callback';
 // API
 const API = '/api';
 const REGISTER_VIEW = '/:id/view';
+const ADD_COMMENT = '/:id/comment';
 
 
 const routes = {
@@ -80,6 +81,7 @@ const routes = {
     me: ME,
     api: API,
     registerView: REGISTER_VIEW,
+    addComment: ADD_COMMENT,
 };
 
 export default routes;
