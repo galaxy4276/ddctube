baf3e999d74de4e9c731f7bd895a076a6db49193
galaxy4276 <galaxyhi4276@gmail.com>
2020-04-08 21:35:29 +0900

    apiRouter 백단 작성 완료 #2

---
+++ routes/apiRouter.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import routes from '../routes';
+import { postRegisterView } from '../controllers/videoController';
+
+const apiRouter = express.Router();
+
+// apiRouter.get(routes.users, users);
+apiRouter.get(routes.registerView, postRegisterView);
+
+
+export default apiRouter;
+
+
+
+
