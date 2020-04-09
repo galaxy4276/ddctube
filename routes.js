8f578936bef4d5dceecfa6fc58a93a0c03fc9a05
galaxy4276 <galaxyhi4276@gmail.com>
2020-04-09 16:44:08 +0900

    댓글 페이크 기능 추가

--- routes.js
+++ routes.js
@@ -37,6 +37,7 @@ const KAKAO_CALLBACK = '/auth/kakao/callback';
 const API = '/api';
 const REGISTER_VIEW = '/:id/view';
 const ADD_COMMENT = '/:id/comment';
+const DELETE_COMMENT = '/:id/comment-delete';
 
 
 const routes = {
@@ -82,6 +83,7 @@ const routes = {
     api: API,
     registerView: REGISTER_VIEW,
     addComment: ADD_COMMENT,
+    deleteComment: DELETE_COMMENT,
 };
 
 export default routes;
